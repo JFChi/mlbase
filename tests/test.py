85efa6d718dfb3092128beddba810de42ac85a96
Zhen Zhang <git@zhen-zhang.com>
2020-03-01 14:03:34 -0800

    Package

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,10 @@
-import jarvis as j
+from mlbase import jarvis as j
 import unittest
 import numpy as np
 
 class TestJarvis(unittest.TestCase):
     def test_read(self):
-        df = j.read('example.csv')
+        df = j.read('tests/example.csv')
         assert np.isnan(df['C'][0]), df['C'][0]
 
 if __name__ == '__main__':
